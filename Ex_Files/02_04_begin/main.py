dd652b91f8d686d356d85f7f3d937c0ddea447d1
Abhinav Prasad Gupta <abhinavbit@yahoo.co.in>
2023-01-12 12:28:53 +0000

    Commit changes to 02_04

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -15,6 +15,9 @@ for name, age in zip(NAMES, AGES):
 for name in reversed(NAMES):
     print(name)
 
+for i, name in enumerate(NAMES):
+    print(f"{i+1} - {name}")
+
 for i in range(5):
     print(i)
 
